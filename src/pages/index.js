0d5766801c37422e14df2785e1ae018e1036379a
Stan Tan <stanley@jirnexu.com>
2018-09-20 18:48:19 +0800

    Learn up to course 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,13 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Learn to design and code React apps</h1>
-    <p>Trying out react and design. Prototype using Framer X later on</p>
-    <Link to="/page-2/">Watch the video</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+       <h1>Learn to design and code React apps</h1>
+       <p>Trying out react and design. Prototype using Framer X later on</p>
+       <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
