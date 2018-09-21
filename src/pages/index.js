a8f31ff46e0b9a5acaff244d87fa6650df79d5ff
Stan Tan <stanley@jirnexu.com>
2018-09-21 11:05:06 +0800

    Followed 4th class animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,8 @@ const IndexPage = () => (
   <Layout>
     <div className="Hero">
       <div className="HeroGroup">
-       <h1>Learn to design and code React apps</h1>
-       <p>Trying out react and design. Prototype using Framer X later on</p>
+       <h1>Learn to<br />design and code React apps</h1>
+       <p>Trying out react and design. <br />Prototype using Framer X later on</p>
        <Link to="/page-2/">Watch the video</Link>
       </div>
     </div>
